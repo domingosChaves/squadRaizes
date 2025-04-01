b386dca6bb54e1c86b116c3173d477772bf93415
Domingos Chaves <dudu.dekst18@gmail.com>
2025-04-01 18:53:11 -0300

    Criando estrutura inicial

---
+++ src/main/java/com/domingos/triboRaizes/TriboRaizesApplication.java
@@ -0,0 +1,13 @@
+package com.domingos.triboRaizes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TriboRaizesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TriboRaizesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/domingos/triboRaizes/TriboRaizesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.domingos.triboRaizes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TriboRaizesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
